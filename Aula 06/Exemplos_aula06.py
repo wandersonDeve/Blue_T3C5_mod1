a422b0be42b6b01e8456cf65c9fe0d21d0b8c1c5
Wanderson Santos <81826043+wandersonDeve@users.noreply.github.com>
2021-06-14 22:53:04 -0300

    estudo de for

--- Aula 06/Exemplos_aula06.py
+++ Aula 06/Exemplos_aula06.py
@@ -5,10 +5,3 @@ nome = 'Wanderson'
 for letra in nome:
     print(f'{letra}', end='',flush=True)
     sleep(0.2)
-print()
-
-
-lista = [10,20,30,40,50]
-for i in range(0,len(lista)):
-    print(f'{lista[i]}',end=',',flush=True)
-    sleep(0.2)
