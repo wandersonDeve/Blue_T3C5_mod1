dcd4ed052a196154af40e4548cf903c130a99ec0
Wanderson Santos <81826043+wandersonDeve@users.noreply.github.com>
2021-06-18 19:12:33 -0300

    changes

--- Aula 10/teste.py
+++ Aula 10/teste.py
@@ -1,1 +1,17 @@
-### Aula 10- Continuação Python ###
+### LIST CONPREHENSION ###
+
+# CASO 01
+lista = [10,20,30,40,50,60,70,80,90,100]
+
+nova_lista = [valor*2 for valor in lista]
+
+print(nova_lista)
+
+# CASO 02
+
+lista1 = [500,1500,2000,100,25]
+
+nova_lista1 = [valor*0.5 for valor in lista1 if valor >= 1000]
+
+print(nova_lista1)
+
