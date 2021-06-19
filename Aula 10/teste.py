ad511c9468b8c95b02e8667700bc4517fb87daf4
Wanderson Santos <81826043+wandersonDeve@users.noreply.github.com>
2021-06-19 18:44:23 -0300

    Versão 1.2

--- Aula 10/teste.py
+++ Aula 10/teste.py
@@ -6,13 +6,11 @@ nova_lista = [valor*2 for valor in lista]
 print(nova_lista)
 
 # CASO 02
-
 lista1 = [500,1500,2000,100,25]
 nova_lista1 = [valor*0.5 for valor in lista1 if valor >= 1000]
 print(nova_lista1)
 
+# CASO 03
 tupla = (1,2,3,4,5)
-
 lista = [i for i in tupla if i%2== 0]
-
 print(lista)
