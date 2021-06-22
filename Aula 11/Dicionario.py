175756a1ec4c66e6725f5dd717f34c1f5a8a815f
Wanderson Santos <81826043+wandersonDeve@users.noreply.github.com>
2021-06-21 22:45:04 -0300

    pop

--- Aula 11/Dicionario.py
+++ Aula 11/Dicionario.py
@@ -3,7 +3,10 @@ vingadores = {'Chris Evans':'Capitão América','Mark Ruffalo':'Hulk','Tom Hiddl
 vingadores['Tom Holland'] = 'Homem-Aranha'
 
 print('=-'*30)
+
 print('Em vingadores Temos:\n')
+
 for k,v in vingadores.items():
     print(f'\033[1;32m{k:<20}\033[m no papel de \033[1;33m{v:<30}\033[m')
+    
 print('=-'*30)
