d6ab328a8f854a8a6d0fe9bcbf9ef64b0563f46d
Wanderson Santos <81826043+wandersonDeve@users.noreply.github.com>
2021-06-23 21:54:03 -0300

    v1.21

--- Aula 12/Projeto 03.py
+++ Aula 12/Projeto 03.py
@@ -2,6 +2,14 @@ from datetime import date
 from time import sleep
 
 def validacao_ano_voto(pergunta):
+    '''
+    :Função validacao_ano_voto: Valida se o ano e o voto é um numero inteiro
+    :varialvel valor: Varialvel com o valor passado pelo usuario convertido em numero inteiro
+    :while: Laço para repetir a pergunta, caso a resposta passada seja invalida
+    :varialvel n: Varialvel com o valor passado pelo usuario como String
+    :param pergunta: Usa uma pergunta enviada por parametro, para o usuario
+    :return: retorna um numero inteiro
+    '''
     valor = 0
     while True:
         n = str(input(pergunta)).strip()
@@ -22,6 +30,12 @@ def validacao_ano_voto(pergunta):
     return valor
 
 def autoriza_voto(ano,voto):
+    '''
+
+    :param ano: ano de nascimento do usuario
+    :param voto: opção de voto do usuario
+    :return: retorna se o usuario esta apto para votar
+    '''
     situacao = True
     idade = date.today().year - ano
     if 18 <= idade < 70:
@@ -36,13 +50,24 @@ def autoriza_voto(ano,voto):
     votacao(situacao,voto)
 
 def votacao(autorizacao,voto):
+    '''
+
+    :param autorizacao: Fornece valor Booleano para a validação do voto
+    :param voto: opção do voto do usuario
+    :return: ---- sem retorno ----
+    '''
     if autorizacao:
         print('\033[1;32mSeu voto foi computado com sucesso.\033[m')
         
     else:
         print('\033[1;31mVocê ainda não tem idade para votar\033[m')
 
-def continuar_votação(condição_de_parada):
+def continuar_votacao(condição_de_parada):
+    '''
+
+    :param condição_de_parada: Condição para parar de aceitar novas pessoas para votar
+    :return: retorna S = sim e N = não, para o programa
+    '''
     continuar = ''
     while True:
         resposta = str(input(condição_de_parada)).strip().replace('ã','a').capitalize()
@@ -59,6 +84,6 @@ while True:
     voto_escolha = validacao_ano_voto('Qual o seu voto:\n\033[1;33m[ 1 ] - Joao\n[ 2 ] - Maria\n[ 3 ] - Jose\n[ 4 ] - Branco\033[m\nSeu voto é: ')
     autoriza_voto(ano_nasci,voto_escolha)
 
-    ctrl = continuar_votação('Deseja continuar ? [N,S] ')
+    ctrl = continuar_votacao('Deseja continuar ? [N,S] ')
     if ctrl == 'N':
         break
